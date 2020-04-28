ec75c6891947685e36bd1e527e9f3e9c4efc8e03
snehalkale41995 <kalesnehal1007@gmail.com>
2020-04-28 18:37:18 +0530

    added fb login

--- src/app/public/login/login.page.ts
+++ src/app/public/login/login.page.ts
@@ -2,15 +2,19 @@ import { Component, OnInit } from '@angular/core';
 import {AuthenticationService} from '../../services/authentication.service'
 import {Router} from '@angular/router';
 import { AlertController } from '@ionic/angular';
-import AppConfig from '../../../appConstants/appConfig.js'
+import AppConfig from '../../../appConstants/appConfig.js';
+import { Facebook, FacebookLoginResponse } from '@ionic-native/facebook/ngx';
+//import { User } from './user';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-  user = {}
-  constructor(private authService : AuthenticationService, private router: Router, public alertController: AlertController) { }
+  user = {email : "", password : ""};
+ // products: Product[] = [];
+  constructor(private authService : AuthenticationService, private router: Router, public alertController: AlertController, private fb: Facebook) { }
 
   ngOnInit() {
   }
@@ -18,13 +22,21 @@ export class LoginPage implements OnInit {
   login(){
     console.log("user", this.user)
     let compRef = this ;
-    if(this.user && this.user.email === AppConfig.userName && this.user.password === AppConfig.password){
     this.router.navigate(['members','dashboard'])
-     this.authService.login();
-    }
-    else{
-      this.presentAlert()
-    }
+      this.authService.login();
+    // if(this.user && this.user.email === AppConfig.userName && this.user.password === AppConfig.password){
+    // this.router.navigate(['members','dashboard'])
+    //  this.authService.login();
+    // }
+    // else{
+    //   this.presentAlert()
+    // }
+  }
+
+  loginFacebook(){
+  this.fb.login(['public_profile', 'user_friends', 'email'])
+  .then((res: FacebookLoginResponse) => console.log('Logged into Facebook!', res))
+  .catch(e => console.log('Error logging into Facebook', e));
   }
 
   async presentAlert() {
